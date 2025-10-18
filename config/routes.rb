3c3e446c440b147c47a461384989bc2b7faba089
Thomas Sycko-Miller <tsycko@tswarren.com>
2025-10-18 21:21:32 +0000

    Fix routes.rb regex

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,20 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  # Health + root
   get  "up", to: "rails/health#show", as: :rails_health_check
   root "home#index"
 
-  # Payments
+  UUID = /[0-9a-f-]{36}/i
+
   namespace :payments do
-    uuid = { public_id: /\A[0-9a-f-]{36}\z/i }
-    resources :ach_routings, param: :public_id, constraints: uuid
+    resources :ach_routings, param: :public_id, constraints: { public_id: UUID }
   end
 
-  # System
   namespace :system do
-    uuid = { public_id: /\A[0-9a-f-]{36}\z/i }
-
-    resources :reference_lists, param: :public_id, constraints: uuid, shallow: true do
-      resources :reference_values, param: :public_id, constraints: uuid
+    resources :reference_lists, param: :public_id, constraints: { public_id: UUID }, shallow: true do
+      resources :reference_values, param: :public_id, constraints: { public_id: UUID }
     end
 
     resources :country_currencies
-    resources :naics_codes, only: [ :index, :show ]
+    resources :naics_codes, only: [:index, :show]
   end
 end
