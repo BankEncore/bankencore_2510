82db5e2c4ae6822f07e2da859c0d57633df82e04
Thomas Sycko-Miller <tsycko@tswarren.com>
2025-10-27 00:31:26 +0000

    Fix system/reference_values controller

--- app/controllers/system/reference_values_controller.rb
+++ app/controllers/system/reference_values_controller.rb
@@ -1,33 +1,18 @@
-# app/controllers/admin/system/reference_values_controller.rb
-class Admin::System::ReferenceValuesController < Admin::BaseController
+# app/controllers/system/reference_values_controller.rb
+class System::ReferenceValuesController < ApplicationController
   before_action :set_list
-  before_action :set_value, only: %i[show edit update destroy]
 
-  def create
-    @value = @list.reference_values.new(value_params)
-    authorize [ :admin, @value ]
-    if @value.save
-      redirect_to admin_system_reference_value_path(@value), notice: "Created"
-    else
-      render :new, status: :unprocessable_content
-    end
+  def index
+    @values = @list.reference_values.order(:sort_index, :code)
+  end
+
+  def show
+    @value = @list.reference_values.find_by!(code: params[:code] || params[:id])
   end
 
   private
 
   def set_list
     @list = System::ReferenceList.find_by!(key: params[:reference_list_key] || params[:reference_list_id] || params[:key])
-  end
-
-  def set_value
-    @value = System::ReferenceValue.find(params[:id]) if params[:id]
-  end
-
-  def value_params
-    params.require(:system_reference_value).permit(
-      :code, :key, :name, :short_name, :description,
-      :sort_index, :active, :external_code, :valid_from, :valid_to,
-      metadata: {}
-    )
   end
 end
