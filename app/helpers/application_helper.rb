e8070ebb832f0e0935567059b86a76faa9ac0677
Thomas Sycko-Miller <tsycko@tswarren.com>
2025-10-27 03:44:06 +0000

    feat(ui): new theme-aware logo, navbar/user menu, and auto-dismissing flash
    * Add horizontal BankEncore SVG partial and integrate in navbar and home hero
    * Rebuild navbar: left logo, center search, right admin+user menu
    * Replace sign-out button with Turbo-friendly link
    * Add cohesive user menu with in-menu theme customizer
    * Introduce flash container + auto-dismiss script; center and widen alerts
    * Tweak head/layout includes and footer to support new components
    * Helper: inline SVG render utility and sizing support
    
    New files:
    
    * app/views/shared/_bankencore_logo_horizontal.html.erb
    * app/views/layouts/_user_menu.html.erb
    * app/views/layouts/_theme_switcher_script.html.erb
    * app/views/layouts/_flash_autodismiss.html.erb
    * app/assets/images/bankencore-horizontal2.svg
    * app/assets/images/logo2.svg
    
    Touchpoints:
    
    * app/assets/images/logo.svg
    * app/helpers/application_helper.rb
    * app/views/home/index.html.erb
    * app/views/layouts/_flash.html.erb
    * app/views/layouts/_footer.html.erb
    * app/views/layouts/_head.html.erb
    * app/views/layouts/_navbar.html.erb
    * app/views/layouts/_theme_switcher.html.erb
    * app/views/layouts/application.html.erb
    
    Refs: ui/theme, auth/logout, flash UX

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
         svg = Rails.root.join("app/assets/images/logo.svg").read
         content_tag(:span, svg.html_safe, **opts)
     end
+
+    def inline_svg(filename, **attrs)
+        path = Rails.root.join("app/assets/images", filename)
+        svg  = File.read(path)
+        if attrs.any?
+        injected = attrs.map { |k, v| %(#{k.to_s.dasherize}="#{ERB::Util.html_escape(v)}") }.join(" ")
+        svg.sub!("<svg", "<svg #{injected}")
+        end
+        svg.html_safe
+    end
 end
